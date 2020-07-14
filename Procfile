6eb125501f006699d12aa205c9370ce494e1be67
poy <poytoday@gmail.com>
2020-07-14 17:03:46 +0700

    edit Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn lineconnect2.wsgi
+web: gunicorn lineconnect.wsgi
--- mylinebot/views.py
+++ mylinebot/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from django.views.decorators.csrf import csrf_exempt
 from linebot import (LineBotApi, WebhookHandler)
 # Create your views here.
 
