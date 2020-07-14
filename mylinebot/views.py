4a404b836bea85b74ae6717fa0a2663de046f4cc
poy <poytoday@gmail.com>
2020-07-14 17:11:26 +0700

    add HttpResponse

--- mylinebot/views.py
+++ mylinebot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseForbidden, HttpResponse
+
 from linebot import (LineBotApi, WebhookHandler)
 # Create your views here.
 
